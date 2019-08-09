18ec13613b242fe87b8103c3a74a82daa37f3681
Reuben Docea <rdocea95@gmail.com>
2019-08-09 20:14:55 +0100

    Added activation function

--- Network Class.py
+++ Network Class.py
@@ -8,7 +8,8 @@ Created on Fri Aug  9 19:14:43 2019
 import numpy as np
 
 class Network:
-    def __init__(self, nIn, nHid, nOut):
+    def __init__(self, nIn, nHid, nOut,activationFunc):
+        self.activationFunc=activationFunc
         
         self.FirstWeights = np.random.rand(nHid,nIn)
         self.SecondWeights = np.random.rand(nOut,nHid)
@@ -21,10 +22,18 @@ class Network:
         
     def ForwardPass(self,Input):
         Hidden=np.dot(self.FirstWeights,Input)+self.FirstBiases
+        Hidden=self.ActivationFunc(Hidden)
         self.Output=np.dot(Hidden,self.SecondWeights)+self.SecondBiases
+        #self.Output=self.ActivationFunc(self.Output)
     
     def ComputeError(self,Label):
         self.Error=np.sum((self.Output-Label)^2)
+        
+    def ActivationFunc(self,Layer):
+        if self.activationFunc == 'Sigmoid'
+            return 1/(1+np.exp(-Layer))
+        
+        
         
     
         
