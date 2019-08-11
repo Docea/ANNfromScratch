corrected derivative of activation function

Main.py
@@ -11,7 +11,7 @@ Created on Sat Aug 10 12:30:20 2019
 import NetworkClass as net
 import numpy as np
 
-Net = net.Network(2,3,2,'Sigmoid',0.01)
+Net = net.Network(2,3,2,'Sigmoid',0.1)
 
 Input = np.random.rand(2,1)
 Label = np.array([[1],[0]])
@@ -29,3 +29,16 @@ Error = Net.Error
 Output = Net.Output
 WeightGrads1 = Net.Weight1Gradients
 WeightGrads2 = Net.Weight2Gradients
+
+op = []
+
+for i in range(1000):
+    Net.ForwardPass(Input)
+    Net.ComputeError(Label)
+    Net.Backprop(Label)
+
+    Net.Update()
+    
+    newWeights = Net.FirstWeights
+    Output = Net.Output
+    

NetworkClass.py
@@ -39,14 +39,14 @@ class Network:
     def Backprop(self,Label):
         dErr_dOut=(self.Output-Label)
         if self.activationFunc=='Sigmoid':
-            dOut_dNet2=self.netOutput*(1-self.netOutput)
+            dOut_dNet2=self.Output*(1-self.Output)
         dNet2_dW2=self.Hidden
         
         self.Weight2Gradients=np.dot(np.multiply(dErr_dOut,dOut_dNet2),np.transpose(dNet2_dW2))
         
         dNet2_dHidden=self.SecondWeights
         if self.activationFunc=='Sigmoid':
-            dHidden_dNet1=self.netHidden*(1-self.netHidden)
+            dHidden_dNet1=self.Hidden*(1-self.Hidden)
         dNet1_dW1=self.Input
         
         self.Weight1Gradients=np.dot(np.multiply(np.transpose(np.dot(np.transpose(np.multiply(dErr_dOut,dOut_dNet2)),self.SecondWeights)),self.Hidden),np.transpose(self.Input))