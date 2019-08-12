4757f210e18a3364372c786d59d892c05991298a
Reuben Docea <rdocea95@gmail.com>
2019-08-12 02:11:47 +0100

    Network converges! But not for large numbers of neurons

--- Main.py
+++ Main.py
@@ -10,15 +10,35 @@ Created on Sat Aug 10 12:30:20 2019
 
 import NetworkClass as net
 import numpy as np
+import matplotlib.pyplot as plt
 
-Net = net.Network(2,3,2,'Sigmoid',0.1)
+"""
+################ Uncomment for Extracting Data from CSVs ######################
+import pandas as pd
+import matplotlib.pyplot as plt
 
-Input = np.random.rand(2,1)
-Label = np.array([[1],[0]])
+trainData = pd.read_csv('train.csv',',')
+trainData = trainData.values
+testData = pd.read_csv('test.csv',',')
+testData = testData.values
+
+sampleInput = trainData[0][1:]
+sampleInput = sampleInput.reshape(len(sampleInput),1)
+sampleLabel = trainData[0][0]
+
+inputSize = len(sampleInput)
+hiddenSize = round(inputSize*1.25)
+outputSize = 1
+learningRate=0.1
+"""
+
+Net = net.Network(3,4,3,'Sigmoid',0.1)
+
+Input = np.array([[0],[1],[0]])
+Label = np.array([[0.2],[0.6],[0.4]])
 Net.ForwardPass(Input)
 Net.ComputeError(Label)
 Net.Backprop(Label)
-
 
 Weights1=Net.FirstWeights
 Weights2=Net.SecondWeights
@@ -31,6 +51,8 @@ WeightGrads1 = Net.Weight1Gradients
 WeightGrads2 = Net.Weight2Gradients
 
 op = []
+op2 = []
+op3 = []
 
 for i in range(1000):
     Net.ForwardPass(Input)
@@ -42,3 +64,11 @@ for i in range(1000):
     newWeights = Net.FirstWeights
     Output = Net.Output
     
+    op.append(Output[0][0])
+    op2.append(Output[1][0])
+    op3.append(Output[2][0])
+    
+plt.plot(op)
+plt.plot(op2)
+plt.plot(op3)
+plt.show()
